28d3bf6c6e00329fc66e7a2484c9b2de2650a942
karroumi23 <anasskarroumi2016@gmail.com>
2025-02-20 11:20:32 +0100

    add some effects

--- include/nav.php
+++ include/nav.php
@@ -7,6 +7,10 @@
   };
 
 ?>
+    <!-- script font-awsome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
@@ -15,8 +19,8 @@
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+        <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNav">
+            <ul class="navbar-nav d-flex gap-1">
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="index.php">Ajouter Utilisateur</a>
                 </li>
@@ -39,19 +43,37 @@
                 <li class="nav-item">
                     <a class="nav-link " aria-current="page" href="deconnexion.php">Deconnexion </a>
                 </li>
+               
+
                 <?php
                 }else{
                     ?>
                 <!-- if not ... -->
-                <li class="nav-item">
-                    <a class="nav-link" href="Connexion.php">Connexion</a>
+                <li class="nav-item ">
+                    <a class="nav-link " href="Connexion.php">Connexion</a>
                 </li>
                 <?php
                 }
                 ?>
 
-
             </ul>
+               <!--  Link to go to the front -->
+            <div>
+                  
+                  <a class="nav-link fs-5 " id="Sitefront" aria-current="page" href="../front/index.php">
+                        <i class="fa-solid fa-cart-shopping"></i>
+                              Site front 
+                  </a>
+                  <style>
+                    #Sitefront{
+                        color: #325FD7;
+                    }
+                     #Sitefront:hover{
+                         color:rgb(45, 60, 100) !important;
+                     }
+                  </style>
+               
+             </div>
         </div>
     </div>
 </nav>
